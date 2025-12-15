3d3c66a967bdd1d91e84668b8c863ec9c785da9c
Luis Arias <luis@refugies.info>
2025-12-15 07:49:10 +0100

    feat: add Button and Header Storybook stories and update badge web component.

---
+++ apps/storybook/.storybook/main.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
+  framework: {
+    name: "@storybook/web-components-vite",
+    options: {},
+  },
+};
+export default config;
