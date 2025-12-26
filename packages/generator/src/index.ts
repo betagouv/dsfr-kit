0d67c251111b601f6380e0884163ca349499ad4f
Luis Arias <luis@refugies.info>
2025-12-26 18:07:30 +0100

    chore: remove legacy generator package

--- packages/generator/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./ejs-parser.js";
-export * from "./js-parser.js";
-export * from "./lit-generator.js";
-export * from "./source-locator.js";
-export * from "./source-locator.js";
-export * from "./style-resolver.js";
